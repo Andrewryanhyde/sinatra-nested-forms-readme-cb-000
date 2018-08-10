8f9e142cf1feb8d965a514e33d858f3889c4b51a
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-08-10 08:26:21 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    erb :new 
+  get '/' do
+    erb :new
   end
+
+  post '/student' do 
+    @student = Student.new(params[:student])
+
+    params[:student][:courses].each do |details| 
+      Course.new(details)
+    end
 
 end
